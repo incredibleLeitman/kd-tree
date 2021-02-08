96e3dfa732ebccc9d50b023a5e0c2c5d0f727170
incredibleLeitman <michael.leithner@gmail.com>
2021-02-08 16:37:52 +0100

    evaluating commmand line arguments

--- main.cpp
+++ main.cpp
@@ -2,36 +2,67 @@
 #ifdef _WIN32 // vis only supported on windows
 	#include "vis.h"
 #endif
+#include "pointGenerator.h"
 
+#include <cstring> // stoi
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
     std::cout << "ALGO assignment \"kd-tree\"\n";
 
-	// TODO:
-	// evaluate params
-	// -p, performance
-	// -f, filename
+	// read program arguments
+	// TODO: care a little more <3 --> use getopt
+	// -v                   mode with enabled visualization
+	// -f <filename>        read triangle points from file
+	// --rngcount <count>   generates count random float values
+	bool vis = true;
+	std::string fileRnd = "";
+	int count = 10;
 
-	// test values
-	std::vector<Point*> points
+	// TODO: add params:
+	// -runs <count>        runs performance mode multiple times
+	for (int i = 0; i < argc; ++i)
 	{
-		new Point(0.0f, 0.0f, 0.0f),
-		new Point(2.0f, 0.0f, 0.0f),
-		new Point(1.0f, 2.0f, 0.0f),
-		new Point(2.0f, 0.0f, 0.0f),
-		new Point(3.5f, 1.0f, 0.0f),
-		new Point(2.5f, 3.0f, 0.0f)
-	};
+		if (strcmp(argv[i], "-v") == 0) vis = true;
+		else if (strcmp(argv[i], "-f") == 0) fileRnd = argv[i + 1];
+		else if (strcmp(argv[i], "--rngcount") == 0) count = std::stoi(argv[i + 1]);
+		//else if (strcmp(argv[i], "--runs") == 0) runs = std::stoi(argv[i + 1]);
+	}
+
+	std::vector<Point*> points;
+	if (fileRnd.empty() == false) // read random points from given file
+	{
+		std::cout << "reading random numbers from file: " << fileRnd << "..." << std::endl;
+		points = PointGenerator::read_file(fileRnd);
+	}
+	else // generate random points
+	{
+		std::cout << "generating " << count << " random numbers" << std::endl;
+		points = PointGenerator::generate_points(count, 0.0, 1000.0);
+
+		// test values
+		/*std::vector<Point*> points
+		{
+			new Point(0.0f, 0.0f, 0.0f),
+			new Point(2.0f, 0.0f, 0.0f),
+			new Point(1.0f, 2.0f, 0.0f),
+			new Point(2.0f, 0.0f, 0.0f),
+			new Point(3.5f, 1.0f, 0.0f),
+			new Point(2.5f, 3.0f, 0.0f)
+		};*/
+	}
 
 	//KDTree *tree = new KDTree(points);
 	KDTree tree(points);
 
-	#ifdef _WIN32
+	if (vis)
+	{
+#ifdef _WIN32
 		Vis vis(tree);
 		vis.display();
-	#else
+#else
 		std::cout << "visualization not supported in non-windows systems" << std::endl;
-	#endif
+#endif
+	}
 }
